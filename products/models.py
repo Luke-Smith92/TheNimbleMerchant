e4695a51c85abba24f6312cd4d49b93b0791428a
Luke-Smith92 <lsmith92@hotmail.co.uk>
2026-06-15 20:10:57 +0100

    Create product and category models

--- products/models.py
+++ products/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    """Product category for grouping store products."""
+
+    name = models.CharField(max_length=254)
+    friendly_name = models.CharField(max_length=254, blank=True, null=True)
+
+    class Meta:
+        verbose_name_plural = 'Categories'
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    """Individual product sold by The Nimble Merchant."""
+
+    category = models.ForeignKey(
+        Category,
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL
+    )
+    name = models.CharField(max_length=254)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    image = models.ImageField(null=True, blank=True)
+    stock_quantity = models.PositiveIntegerField(default=0)
+
+    def __str__(self):
+        return self.name
