c40b0f24fe97e588ef00bb9b30e69210dc3a0bbd
Luke-Smith92 <lsmith92@hotmail.co.uk>
2026-06-20 10:39:36 +0100

    Create order models for checkout

--- checkout/models.py
+++ checkout/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from products.models import Product
 
-# Create your models here.
+
+class Order(models.Model):
+    full_name = models.CharField(max_length=50)
+    email = models.EmailField(max_length=254)
+    phone_number = models.CharField(max_length=20)
+    address = models.CharField(max_length=254)
+    town_or_city = models.CharField(max_length=40)
+    postcode = models.CharField(max_length=20)
+    date = models.DateTimeField(auto_now_add=True)
+    order_total = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+
+    def __str__(self):
+        return f"Order {self.id}"
+
+
+class OrderLineItem(models.Model):
+    order = models.ForeignKey(Order, null=False, blank=False, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, null=False, blank=False, on_delete=models.CASCADE)
+    quantity = models.IntegerField(default=1)
+
+    def __str__(self):
+        return f"{self.quantity} x {self.product.name}"
