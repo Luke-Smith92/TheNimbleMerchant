abf101f6a3ad87a46dcc708a69347570b8229923
Luke-Smith92 <lsmith92@hotmail.co.uk>
2026-06-20 09:52:36 +0100

    Create basic shopping cart functionality

--- cart/views.py
+++ cart/views.py
@@ -1,3 +1,43 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from products.models import Product
 
-# Create your views here.
+
+def view_cart(request):
+    """Display the shopping cart."""
+    cart = request.session.get('cart', {})
+    cart_items = []
+    total = 0
+
+    for product_id, quantity in cart.items():
+        product = Product.objects.get(id=product_id)
+        subtotal = product.price * quantity
+        total += subtotal
+
+        cart_items.append({
+            'product': product,
+            'quantity': quantity,
+            'subtotal': subtotal,
+        })
+
+    context = {
+        'cart_items': cart_items,
+        'total': total,
+    }
+
+    return render(request, 'cart/cart.html', context)
+
+
+def add_to_cart(request, product_id):
+    """Add a product to the shopping cart."""
+    quantity = int(request.POST.get('quantity'))
+
+    cart = request.session.get('cart', {})
+
+    if str(product_id) in cart:
+        cart[str(product_id)] += quantity
+    else:
+        cart[str(product_id)] = quantity
+
+    request.session['cart'] = cart
+
+    return redirect('view_cart')
