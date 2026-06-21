dcdd8a039bf1fddd161f82188dde36a966a7c217
Luke-Smith92 <lsmith92@hotmail.co.uk>
2026-06-21 17:59:26 +0100

    Implement user accounts, order history, email notifications and site styling

--- nimble_merchant/settings.py
+++ nimble_merchant/settings.py
@@ -121,6 +121,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]
 STRIPE_PUBLIC_KEY = ""
 STRIPE_SECRET_KEY = ""
 STRIPE_CURRENCY = "gbp"
