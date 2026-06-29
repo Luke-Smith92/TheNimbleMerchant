f9bab6200acf2cd632e487cce56fa0441196cb52
Luke-Smith92 <lsmith92@hotmail.co.uk>
2026-06-29 20:17:03 +0100

    Configure environment variables for production deployment

--- nimble_merchant/settings.py
+++ nimble_merchant/settings.py
@@ -26,7 +26,7 @@ if os.path.exists("env.py"):
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DEBUG", "False") == "True"
 
 ALLOWED_HOSTS = [
     "127.0.0.1",
